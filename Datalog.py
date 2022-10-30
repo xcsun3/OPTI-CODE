3255b8dd8dd6dc82701d29de4b2233983251d98e
xcsun3 <112048149+xcsun3@users.noreply.github.com>
2022-10-30 14:19:40 -0400

    Update Datalog.py

--- Datalog.py
+++ Datalog.py
@@ -1,57 +1,57 @@
 import time
+from xmlrpc.client import boolean
 # import Accelerometer
 import BatteryManagement
 import gps
 import pandas as pd
-from datetime import date
+from datetime import datetime
 from threading import Timer
 
 
 class Datalog():
 
-    def __init__(self, interval: int):
-        self.cur_time = time.time()
+    def __init__(self, interval = 1, export = True):
         self._timer = None
         self.interval = interval
-        self.today = date.today()
-        self.data = {'Time': [], 'Battery': [], 'AccX': [],
-                     'AccY': [], 'GPS': []}
+        self.export = export
+        self.today = datetime.today()
+        self.data = {'Time': [], 'Battery': []}
         self.is_running = False
         self.start()
+        self.get_bat = BatteryManagement.BatteryManagement()
+        # self.get_acc = Accelerometer.Accelerometer()
+        # self.get_gps = gps.BerryGPS()
 
     # Get accelerometer information
-    # smbus library used in IMU only works on python 3.5
     # def get_acceleration(self):
-        # get_acc = Accelerometer.Accelerometer()
-        # accXnorm, accYnorm = get_acc.get_measurement()
+        # accXnorm, accYnorm = self.get_acc.get_measurement()
         # return accXnorm, accYnorm
 
     # Get battery information
     def get_batterty(self):
-        get_bat = BatteryManagement.BatteryManagement()
-        battery = get_bat.get_measurement()
+        battery = self.get_bat.get_BSoC()
         return battery
 
     # Get gps information
-    def get_gps(self):
-        get_gps = gps.BerryGPS()
-        res = get_gps.getData()
-        lat, dirLat, lon, dirLon, speed, time, trCourse, date = res
-        return res
+    # def get_gps(self):
+        # res = self.get_gps.getData()
+        # lat, dirLat, lon, dirLon, speed, time, trCourse, date = res
+        # return res
 
     # What to do in each time step
     def _run(self):
         self.is_running = False
         self.start()
-        self.cur_time = time.time()
+        now = datetime.now()
+        self.current_time = now.strftime("%H:%M:%S")
         # data_x, data_y = self.get_acceleration()
         data_bat = self.get_batterty()
-        data_gps = self.get_gps()
-        self.data['Time'].append(self.cur_time)
+        # data_gps = self.get_gps()
+        self.data['Time'].append(self.current_time)
         # self.data['AccX'].append(data_x)
         # self.data['AccY'].append(data_y)
         self.data['Battery'].append(data_bat)
-        self.data['GPS'].append(data_gps)
+        # self.data['GPS'].append(data_gps)
 
     # Start data collection
     def start(self):
@@ -64,11 +64,14 @@ class Datalog():
     def stop(self):
         self._timer.cancel()
         self.is_running = False
+        if self.export:
+            self.output_csv()
 
     # Output saved data of the current run in csv
     def output_csv(self):
         df = pd.DataFrame(self.data)
-        d4 = self.today.strftime("%b-%d-%Y")
-        file_name = "CMSR_log_" + d4
+        df['Time'] = pd.to_datetime(df['Time'])
+        d4 = self.today.strftime("%b-%d-%Y-%H-%M")
+        file_name = "CMSR_log_" + d4 + ".csv"
         df.to_csv(file_name, encoding='utf-8', index=False)
         return True
